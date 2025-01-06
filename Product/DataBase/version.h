877226d5f78b463c22491d6574596b2eb5b7aeab
akb07mdt <anik.medtronic.640266@gmail.com>
2025-01-06 12:40:16 +0530

    here is new test

--- Product/DataBase/version.h
+++ Product/DataBase/version.h
@@ -2,12 +2,12 @@
 //
 // Holds Version Strings for this product.
 //
-// Use the tag TM9101_20250106_070527_Enter
+// Use the tag TM9101_20250106_071002_10_42_6
 //   to checkout code associated with this version.
 
 #define PRODUCT_NAME    "TM9101"
 #define PRODUCT_DATE    "20250106"
-#define PRODUCT_TIME    "070527"
-#define PRODUCT_TAG     "TM9101_20250106_070527_Enter"
-#define PRODUCT_VERSION "Enter"
+#define PRODUCT_TIME    "071002"
+#define PRODUCT_TAG     "TM9101_20250106_071002_10_42_6"
+#define PRODUCT_VERSION "10.42.6"
 
