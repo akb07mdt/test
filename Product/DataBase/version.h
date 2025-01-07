6d97ffe190465fd85db1b5ba74eaeb21ab77de7c
akb07mdt <anik.medtronic.640266@gmail.com>
2025-01-07 10:50:55 +0530

    8th Commit

--- Product/DataBase/version.h
+++ Product/DataBase/version.h
@@ -2,13 +2,13 @@
 //
 // Holds Version Strings for this product.
 //
-// Use the tag TM9101_20250107_051438_10_20_30_40_50_60
+// Use the tag TM9101_20250107_052028_10_20_30_40_50_60_70
 //   to checkout code associated with this version.
 //
 
 #define PRODUCT_NAME    "TM9101"
 #define PRODUCT_DATE    "20250107"
-#define PRODUCT_TIME    "051438"
-#define PRODUCT_TAG     "TM9101_20250107_051438_10_20_30_40_50_60"
-#define PRODUCT_VERSION "10.20.30.40.50.60"
+#define PRODUCT_TIME    "052028"
+#define PRODUCT_TAG     "TM9101_20250107_052028_10_20_30_40_50_60_70"
+#define PRODUCT_VERSION "10.20.30.40.50.60.70"
 
