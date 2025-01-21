70f8d5f67ca2ffd034ff60d2796d7f4113d87fa1
akb07mdt <anik.medtronic.640266@gmail.com>
2025-01-21 10:18:14 +0530

    hi

--- Product/DataBase/version.h
+++ Product/DataBase/version.h
@@ -2,12 +2,12 @@
 //
 // Holds Version Strings for this product.
 //
-// Use the tag TM9101_20250120_180920_2_3_1
+// Use the tag TM9101_20250121_044800_1_2_7
 // to checkout code associated with this version.
 //
 
 #define PRODUCT_NAME    TM9101
-#define PRODUCT_DATE    20250120
-#define PRODUCT_TIME    180920
-#define PRODUCT_TAG     TM9101_20250120_180920_2_3_1
-#define PRODUCT_VERSION 2.3.1
+#define PRODUCT_DATE    20250121
+#define PRODUCT_TIME    044800
+#define PRODUCT_TAG     TM9101_20250121_044800_1_2_7
+#define PRODUCT_VERSION 1.2.7
