4d8d3efc256d7ca857b366dbf67d3cb45044c6d7
akb07mdt <anik.medtronic.640266@gmail.com>
2025-01-21 10:37:37 +0530

    <Replace this text with commit details and then save and exit the editor>

--- Product/DataBase/version.h
+++ Product/DataBase/version.h
@@ -2,12 +2,12 @@
 //
 // Holds Version Strings for this product.
 //
-// Use the tag TM9101_20250121_050221_1_2_9
+// Use the tag TM9101_20250121_050713_1_0_2
 // to checkout code associated with this version.
 //
 
 #define PRODUCT_NAME    TM9101
 #define PRODUCT_DATE    20250121
-#define PRODUCT_TIME    050221
-#define PRODUCT_TAG     TM9101_20250121_050221_1_2_9
-#define PRODUCT_VERSION 1.2.9
+#define PRODUCT_TIME    050713
+#define PRODUCT_TAG     TM9101_20250121_050713_1_0_2
+#define PRODUCT_VERSION 1.0.2
